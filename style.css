1e421ba35a97d4a0ba363d9153dc6a5a42b0e737
Murilo Quartz <153753008+Muriloxox@users.noreply.github.com>
2025-01-17 19:21:30 -0300

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,46 @@
+body {
+    margin: 0;
+    overflow: hidden;
+    background: linear-gradient(to bottom, #003973, #005f99, #0098c8, #00b3d9, #00d9ff);
+    cursor: none;
+    height: 100vh;
+}
 
+.pixel-art {
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    pointer-events: none;
+    image-rendering: pixelated;
+    transform-origin: 50% 50%;
+}
+
+footer {
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+    font-size: 12px;
+    color: rgba(0, 0, 0, 0.548);
+    text-align: right;
+}
+
+.bubble {
+    position: absolute;
+    bottom: 0;
+    background: rgba(255, 255, 255, 0.8);
+    border-radius: 50%;
+    pointer-events: none;
+    image-rendering: pixelated;
+    transform: translateY(100%);
+}
+
+@keyframes bubble-rise {
+    0% {
+        transform: translateY(100%);
+        opacity: 1;
+    }
+    100% {
+        transform: translateY(-100vh);
+        opacity: 0;
+    }
+}
